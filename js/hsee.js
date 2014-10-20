5fc3e6b2b2159b06e33de6aca6e133403161b768
xiaojin <j.xiao.edin@gmail.com>
2014-10-20 21:42:20 +0800

    update first page css3 animation bugs

--- js/hsee.js
+++ js/hsee.js
@@ -26,32 +26,28 @@ var hsee = {
         hsee.bind();
         var swiperParent = new Swiper('.swiper-parent', {
             mode : 'vertical',
-            speed: 750,
-            paginationClickable : true,
-            slidesPerView :1,
-            onSlideChangeStart: function(swiper,direction){
-
-                 if(swiper.activeIndex === 1 && direction ==="next"){
-                     $(".block-recruit").addClass("recruitToNext");
-                 }
+            speed : 750,
+            slidesPerView : 1,
+            onSlideChangeStart : function(swiper, direction) {
+                if (swiper.activeIndex === 1 && direction === "next") {
+                    $(".block-recruit").addClass("recruitToNext");
+                }
             },
-            onTouchMove : function(swiper){
-                var offsetY = -swiper.getWrapperTranslate('y') ,
-                silderHalfHeighter = $(".page1").height()*0.3;
+            onTouchMove : function(swiper) {
+                var offsetY = -swiper.getWrapperTranslate('y'), silderHalfHeighter = $(".page1").height() * 0.3;
 
-                if(swiper.activeIndex === 0){                    
-                    if ( offsetY > silderHalfHeighter) {
+                if (swiper.activeIndex === 0) {
+                    if (offsetY > silderHalfHeighter) {
                         $(".block-recruit").addClass("recruitToNext");
                     };
                 }
             },
-            onSlideChangeEnd:function(swiper,direction){
-                if(swiper.activeIndex === 1){ 
+            onSlideChangeEnd : function(swiper, direction) {
+                if (swiper.activeIndex === 1) {
                     $(".block-recruit").removeClass("recruitToNext");
-                    }                 
+                }
             }
         });
-       
         var swiperNested1 = new Swiper('.swiper-human', {
             paginationClickable : true,
              mode : 'horizontal',
@@ -61,59 +57,59 @@ var hsee = {
                 
 
             },
-             onSetWrapperTransform: function(swiper,transform){
-                 var index= swiper.activeIndex;
-                 var $facer =$(".facer"+index),
-                     per = (transform.x+(swiper.width*index))/swiper.width,
-                     $facel =  $(".facel"+index);
-                 console.log(swiper.positions);
-                 var isLong = Math.abs(swiper.positions.current - swiper.positions.start);
-                    if ((index + 1) < 6 && (swiper.positions.current - swiper.positions.start) < 0 ) {
-                        var $nrface = $(".facer" + (index + 1)), $nlface = $(".facel" + (index + 1));
-                            $nrface.show();                   
-                        if (Math.abs(per) < 0.45) {                          
-                            $facer.css("transform","rotateY(" + (per * 2) * 90 + "deg)");
-                        } else if (Math.abs(per) > 0.5) {
-                            var degree = ((1 - Math.abs(per)) * 2) * 90;
-                            //hide current right face
-                            $facer.hide();
-                            //  show up nect left face
-                            $nlface.show();
-                            $facel.css("z-index", 0);
-                            $nlface.css("z-index", 1);
-                            $nlface.css("transform","rotateY(" + degree + "deg)");
-                            if (degree < 10) {
-                                $facel.hide();
-                            }
-                        }
-                    } else if ((swiper.positions.current - swiper.positions.start) > 0 && (index - 1) >= 0) {
-                        var $prface = $(".facer" + (index - 1)), $plface = $(".facel" + (index - 1));
-                        $plface.show();
-                        if (Math.abs(per) < 0.45) {
-                            $facel[0].style.transform = "rotateY(" + (per * 2) * 90 + "deg)";
-                        } else if (Math.abs(per) > 0.5) {
-                            var degree = ((1 - Math.abs(per)) * 2) * 90;
-                            $facel.hide();
-                            //  show up nect left face
-                            $prface.show();
-                            $facer.css("z-index", 0);
-                            $prface.css("z-index", 1);
-                            $prface[0].style.transform = "rotateY(" + degree + "deg)";
-                            if (degree < 10) {
-                                $facer.hide();
-                            }
-                        }
-                    }
-            },    
-            onTouchEnd :function(swiper){
-                console.log(swiper.getWrapperTranslate('x'));
-                var index = Math.abs(swiper.activeIndex);
-                $(".faces").hide();
-                $(".facel"+index).show();
-                $(".facel"+index).css("transform","rotateY(0deg)");
-                $(".facer"+index).show();
-                $(".facer"+index).css("transform","rotateY(0deg)");
-            },
+             // onSetWrapperTransform: function(swiper,transform){
+                 // var index= swiper.activeIndex;
+                 // var $facer =$(".facer"+index),
+                     // per = (transform.x+(swiper.width*index))/swiper.width,
+                     // $facel =  $(".facel"+index);
+                 // console.log(swiper.positions);
+                 // var isLong = Math.abs(swiper.positions.current - swiper.positions.start);
+                    // if ((index + 1) < 6 && (swiper.positions.current - swiper.positions.start) < 0 ) {
+                        // var $nrface = $(".facer" + (index + 1)), $nlface = $(".facel" + (index + 1));
+                            // $nrface.show();                   
+                        // if (Math.abs(per) < 0.45) {                          
+                            // $facer.css("transform","rotateY(" + (per * 2) * 90 + "deg)");
+                        // } else if (Math.abs(per) > 0.5) {
+                            // var degree = ((1 - Math.abs(per)) * 2) * 90;
+                            // //hide current right face
+                            // $facer.hide();
+                            // //  show up nect left face
+                            // $nlface.show();
+                            // $facel.css("z-index", 0);
+                            // $nlface.css("z-index", 1);
+                            // $nlface.css("transform","rotateY(" + degree + "deg)");
+                            // if (degree < 10) {
+                                // $facel.hide();
+                            // }
+                        // }
+                    // } else if ((swiper.positions.current - swiper.positions.start) > 0 && (index - 1) >= 0) {
+                        // var $prface = $(".facer" + (index - 1)), $plface = $(".facel" + (index - 1));
+                        // $plface.show();
+                        // if (Math.abs(per) < 0.45) {
+                            // $facel[0].style.transform = "rotateY(" + (per * 2) * 90 + "deg)";
+                        // } else if (Math.abs(per) > 0.5) {
+                            // var degree = ((1 - Math.abs(per)) * 2) * 90;
+                            // $facel.hide();
+                            // //  show up nect left face
+                            // $prface.show();
+                            // $facer.css("z-index", 0);
+                            // $prface.css("z-index", 1);
+                            // $prface[0].style.transform = "rotateY(" + degree + "deg)";
+                            // if (degree < 10) {
+                                // $facer.hide();
+                            // }
+                        // }
+                    // }
+            // },    
+            // onTouchEnd :function(swiper){
+                // console.log(swiper.getWrapperTranslate('x'));
+                // var index = Math.abs(swiper.activeIndex);
+                // $(".faces").hide();
+                // $(".facel"+index).show();
+                // $(".facel"+index).css("transform","rotateY(0deg)");
+                // $(".facer"+index).show();
+                // $(".facer"+index).css("transform","rotateY(0deg)");
+            // },
         });
         var swiperNested2 = new Swiper('.swiper-mile',{
             paginationClickable : true,
