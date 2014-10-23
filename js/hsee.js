5e57378d843ee8826dd58c3f7b8ce31b1f729ce9
xiaojin <j.xiao.edin@gmail.com>
2014-10-23 14:01:15 +0800

    page02 big changes animation modification version0.1

--- js/hsee.js
+++ js/hsee.js
@@ -1,26 +1,29 @@
 var hsee = {
     bind : function() {
-    
+
     },
-   ready : function() {
-        setTimeout(function() { $('.loading').fadeOut(500); }, 1000);
+    ready : function() {
+        setTimeout(function() {
+            $('.loading').fadeOut(500);
+        }, 1000);
         hsee.bind();
-         if($(window).height() < '500') {
+        var swiperNested1;
+        if ($(window).height() < '500') {
             // 上下滚动翻页
-            $('.swiper-parent').css("min-height","504px");
-            $('.pages').css("min-height","504px");
+            $('.swiper-parent').css("min-height", "504px");
+            $('.pages').css("min-height", "504px");
             var swiperParent = new Swiper('.swiper-parent', {
                 mode : 'vertical',
                 speed : 750,
                 slidesPerView : 1,
-                calculateHeight:true,
+                calculateHeight : true,
                 onSlideChangeStart : function(swiper, direction) {
                     if (swiper.activeIndex === 1 && direction === "next") {
                         $(".block-recruit").addClass("recruitToNext");
                     }
                 },
                 onTouchMove : function(swiper) {
-                    var offsetY = -swiper.getWrapperTranslate('y'), silderHalfHeighter = $(".page1").height() * 0.3;   
+                    var offsetY = -swiper.getWrapperTranslate('y'), silderHalfHeighter = $(".page1").height() * 0.3;
                     if (swiper.activeIndex === 0) {
                         if (offsetY > silderHalfHeighter) {
                             $(".block-recruit").addClass("recruitToNext");
@@ -28,12 +31,16 @@ var hsee = {
                     }
                 },
                 onSlideChangeEnd : function(swiper, direction) {
+                    console.log(swiper.activeIndex);
                     if (swiper.activeIndex === 1) {
                         $(".block-recruit").removeClass("recruitToNext");
+                        swiperNested1.reInit();
+                        swiperNested1.params.autoplay = 300;
+                        swiperNested1.startAutoplay();
                     }
                 }
-            });            
-          } else {
+            });
+        } else {
             var swiperParent = new Swiper('.swiper-parent', {
                 mode : 'vertical',
                 speed : 750,
@@ -45,7 +52,7 @@ var hsee = {
                 },
                 onTouchMove : function(swiper) {
                     var offsetY = -swiper.getWrapperTranslate('y'), silderHalfHeighter = $(".page1").height() * 0.3;
-    
+
                     if (swiper.activeIndex === 0) {
                         if (offsetY > silderHalfHeighter) {
                             $(".block-recruit").addClass("recruitToNext");
@@ -53,121 +60,130 @@ var hsee = {
                     }
                 },
                 onSlideChangeEnd : function(swiper, direction) {
+                    console.log(swiper.activeIndex);
                     if (swiper.activeIndex === 1) {
                         $(".block-recruit").removeClass("recruitToNext");
+                        swiperNested1.params.autoplay = 300;
+                        swiperNested1.startAutoplay();
                     }
                 }
-            });               
-          }
-
-        
-        
-        var swiperNested1 = new Swiper('.swiper-human', {
-            paginationClickable : true,
-             mode : 'horizontal',
+            });
+        }
+        var previousX = 1;
+        swiperNested1 = new Swiper('.swiper-human', {
+            mode : 'horizontal',
             slidesPerView : 1,
-            speed:1050,
-            onTouchMove : function(swiper){  
-            },
-             onSetWrapperTransform: function(swiper,transform){
-                 var index= swiper.activeIndex;
-                 var $facer =$(".facer"+index),
-                     per = (transform.x+(swiper.width*index))/swiper.width,
-                     $facel =  $(".facel"+index);
-                 console.log(swiper.positions);
-                 var isLong = Math.abs(swiper.positions.current - swiper.positions.start);
-                    if ((index + 1) < 6 && (swiper.positions.current - swiper.positions.start) < 0 ) {
-                        var $nrface = $(".facer" + (index + 1)), $nlface = $(".facel" + (index + 1));
-                            $nrface.show();                   
-                        if (Math.abs(per) < 0.45) {                          
-                            $facer.css("-webkit-transform","rotateY(" + (per * 2) * 90 + "deg)");
-                        } else if (Math.abs(per) > 0.5) {
-                            var degree = ((1 - Math.abs(per)) * 2) * 90;
-                            //hide current right face
-                            $facer.hide();
-                            //  show up nect left face
-                            $nlface.show();
-                            $facel.css("z-index", 0);
-                            $nlface.css("z-index", 1);
-                            $nlface.css("-webkit-transform","rotateY(" + degree + "deg)");
-                            if (degree < 10) {
-                                $facel.hide();
-                            }
-                        }
-                    } else if ((swiper.positions.current - swiper.positions.start) > 0 && (index - 1) >= 0) {
-                        var $prface = $(".facer" + (index - 1)), $plface = $(".facel" + (index - 1));
-                        $plface.show();
-                        if (Math.abs(per) < 0.45) {
-                            $facel.css("-webkit-transform", "rotateY(" + (per * 2) * 90 + "deg)");
-                        } else if (Math.abs(per) > 0.5) {
-                            var degree = ((1 - Math.abs(per)) * 2) * 90;
-                            $facel.hide();
-                            //  show up nect left face
-                            $prface.show();
-                            $facer.css("z-index", 0);
-                            $prface.css("z-index", 1);
-                            $prface.css("-webkit-transform","rotateY(" + degree + "deg)");
-                            if (degree < 10) {
-                                $facer.hide();
-                            }
-                        }
-                    }
-            },    
-            onTouchEnd :function(swiper){
+            speed : 400,
+            autoplayStopOnLast : true,
+            simulateTouch : false,
+            onTouchEnd : function(swiper) {
                 console.log(swiper.getWrapperTranslate('x'));
                 var index = Math.abs(swiper.activeIndex);
                 $(".faces").hide();
-                $(".facel"+index).show();
-                $(".facel"+index).css("-webkit-transform","rotateY(0deg)");
-                $(".facer"+index).show();
-                $(".facer"+index).css("-webkit-transform","rotateY(0deg)");
+                $(".facel" + index).show();
+                $(".facel" + index).css("-webkit-transform", "rotateY(0deg)");
+                $(".facer" + index).show();
+                $(".facer" + index).css("-webkit-transform", "rotateY(0deg)");
+            },
+            onSetWrapperTransition : function(swiper, duration) {
+                console.log();
+                if (swiper.positions.current - swiper.positions.start != 0) {
+                    event.preventDefault();
+                    return false;
+                }
+                var transform = swiper.getWrapperTranslate('x');
+                var instance = transform - previousX;
+                previousX = transform;
+                console.log(instance);
+                var index = swiper.activeIndex;
+                var $facer = $(".facer" + index), per = (transform + (swiper.width * index)) / swiper.width, $facel = $(".facel" + index);
+                console.log(transform);
+                if ((index + 1) < 6 && instance < 0) {
+                    var $nrface = $(".facer" + (index + 1)), $nlface = $(".facel" + (index + 1));
+                    $nrface.show();
+                    $nlface.css("-webkit-transform", "rotateY(" + 90 + "deg)");
+                    $facer.css("-webkit-transform", "rotateY(" + -90 + "deg)");
+                    $facer.css("-webkit-transition", "-webkit-transform 0.2s");
+                    setTimeout(function() {
+                        $nlface.show();
+                        $facel.css("z-index", 0);
+                        $nlface.css("z-index", 1);
+                        $nlface.css("-webkit-transform", "rotateY(" + 0 + "deg)");
+                        $nlface.css("-webkit-transition", "-webkit-transform 0.2s");
+                    }, 200);
+                    setTimeout(function() {
+                        $facer.css("-webkit-transform", "rotateY(" + 0 + "deg)");
+                        $facel.css("-webkit-transform", "rotateY(" + 0 + "deg)");
+                        $facer.hide();
+                        $facel.hide();
+                    }, 200);
+                } else if ((index - 1) >= 0 && instance > 0) {
+                    var $prface = $(".facer" + (index - 1)), $plface = $(".facel" + (index - 1));
+                    $plface.show();
+                    $prface.css("-webkit-transform", "rotateY(" + 90 + "deg)");
+                    $facer.css("-webkit-transform", "rotateY(" + -90 + "deg)");
+                    $facer.css("-webkit-transition", "-webkit-transform 0.2s");
+                    setTimeout(function() {
+                        $prface.show();
+                        $facer.css("z-index", 0);
+                        $prface.css("z-index", 1);
+                        $prface.css("-webkit-transform", "rotateY(" + 0 + "deg)");
+                        $prface.css("-webkit-transition", "-webkit-transform 0.2s");
+                    }, 200);
+                    setTimeout(function() {
+                        $facer.hide();
+                        $facel.hide();
+                        $facer.css("-webkit-transform", "rotateY(" + 0 + "deg)");
+                        $facel.css("-webkit-transform", "rotateY(" + 0 + "deg)");
+                    }, 200);
+                }
+
             },
         });
-        var swiperNested2 = new Swiper('.swiper-mile',{
+        var swiperNested2 = new Swiper('.swiper-mile', {
             paginationClickable : true,
             slidesPerView : 1,
-            onSetWrapperTransform: function(swiper,transform){
-                var $mileIcons = $(".mile-icons"),
-                    index= swiper.activeIndex;
-                var per = Math.abs((transform.x+(swiper.width*index))/swiper.width);
+            onSetWrapperTransform : function(swiper, transform) {
+                var $mileIcons = $(".mile-icons"), index = swiper.activeIndex;
+                var per = Math.abs((transform.x + (swiper.width * index)) / swiper.width);
                 var moveDist = (swiper.positions.current - swiper.positions.start);
                 console.log(swiper.positions);
-                if ((index + 1) < 6 && (swiper.positions.current - swiper.positions.start) < 0 ) {
-                        var offsetX = "translateX("+(-per*20-(20*index))+"%)";
-                        console.log(offsetX);
-                        $mileIcons.css('-webkit-transform',offsetX);
-                }  else if ((swiper.positions.current - swiper.positions.start) > 0 && (index - 1) >= 0) {
-                        var offsetX = "translateX("+(per*20-(20*index))+"%)";
-                        $mileIcons.css('-webkit-transform',offsetX);
+                if ((index + 1) < 6 && (swiper.positions.current - swiper.positions.start) < 0) {
+                    var offsetX = "translateX(" + (-per * 20 - (20 * index)) + "%)";
+                    console.log(offsetX);
+                    $mileIcons.css('-webkit-transform', offsetX);
+                } else if ((swiper.positions.current - swiper.positions.start) > 0 && (index - 1) >= 0) {
+                    var offsetX = "translateX(" + (per * 20 - (20 * index)) + "%)";
+                    $mileIcons.css('-webkit-transform', offsetX);
                 }
             },
-            onTouchEnd :function(swiper){
+            onTouchEnd : function(swiper) {
                 console.log(swiper.getWrapperTranslate('x'));
-                var index = swiper.activeIndex,
-                    offset = 0;
-                if(swiper.getWrapperTranslate('x') <0){
-                    offset = "translateX("+((-20*index))+"%)";
-                } else 
-                {
-                    offset = "translateX("+((20*index))+"%)";
-                } 
+                var index = swiper.activeIndex, offset = 0;
+                if (swiper.getWrapperTranslate('x') < 0) {
+                    offset = "translateX(" + ((-20 * index)) + "%)";
+                } else {
+                    offset = "translateX(" + ((20 * index)) + "%)";
+                }
 
-                 $(".mile-icons").css({'-webkit-transform':offset});
+                $(".mile-icons").css({
+                    '-webkit-transform' : offset
+                });
             }
         });
 
         $("#top").click(function() {
             $(".first-class").show();
             $(".first-class").animate({
-                top:0
+                top : 0
             });
-            
-            setTimeout(function(){
+
+            setTimeout(function() {
                 swiperParent.swipeTo(0, 0, true);
-                 $(".first-class").hide();
-            },2000);
-           
-        }); 
+                $(".first-class").hide();
+            }, 2000);
+
+        });
 
     }
 };
